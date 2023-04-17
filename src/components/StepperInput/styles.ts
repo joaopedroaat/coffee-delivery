484d3b5cb98ed7ce0f9be7cd032d06d4da6c27b5
João Pedro <jotapedro1231@gmail.com>
2023-04-17 15:39:40 -0300

    StepperInput styles

--- src/components/StepperInput/styles.ts
+++ src/components/StepperInput/styles.ts
@@ -1,32 +1,43 @@
 import styled from 'styled-components'
 
-export const StepperInputContainer = styled.div``
+export const StepperInputContainer = styled.div`
+  display: flex;
+  background: ${(props) => props.theme['base-button']};
+  border-radius: 6px;
+`
 
 const Button = styled.button`
-  border: none;
-
   background: ${(props) => props.theme['base-button']};
-
-  padding: 0.5rem;
-
+  color: ${(props) => props.theme.purple};
+  border: none;
   cursor: pointer;
 `
 
 export const DecreaseButton = styled(Button)`
+  padding: 0.5rem 0 0.5rem 0.5rem;
   border-radius: 6px 0 0 6px;
 `
 
 export const IncreaseButton = styled(Button)`
+  padding: 0.5rem 0.5rem 0.5rem 0;
   border-radius: 0 6px 6px 0;
 `
 
 export const Input = styled.input.attrs({
   type: 'number',
 })`
-  background: ${(props) => props.theme['base-button']};
-  outline: none;
-  border: none;
+  -moz-appearance: none;
+  appearance: none;
 
-  padding: 0.5rem 0;
   text-align: center;
+  border: none;
+  background: ${(props) => props.theme['base-button']};
+
+  outline: none;
+
+  &::-webkit-outer-spin-button,
+  &::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
 `
