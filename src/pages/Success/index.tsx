982a6e8efac1b8a727d41546b5886bdc3b53fb35
joaopedroaat <jotapedro1231@gmail.com>
2023-04-30 20:29:42 -0300

    Added html structure to Success page

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,14 +1,51 @@
 import { useContext } from 'react'
-import { SuccessContainer } from './styles'
+import {
+  DeliveryInfoBox,
+  DeliveryInfoContainer,
+  SuccessContainer,
+} from './styles'
 import { CheckoutContext } from '../../contexts/CheckoutContext'
+import { CurrencyDollar, MapPin, Timer } from '@phosphor-icons/react'
 
 export function Success() {
   const { address, paymentType } = useContext(CheckoutContext)
 
   return (
     <SuccessContainer>
-      <p>{JSON.stringify(address)}</p>
-      <p>{paymentType}</p>
+      <h1>Uhu! Pedido confirmado</h1>
+      <p>Agora é só aguardar que logo o café chegará até você</p>
+      <DeliveryInfoContainer>
+        <DeliveryInfoBox>
+          <li>
+            <i>
+              <MapPin weight="fill" />
+            </i>
+            <p>
+              Entrega em {address.street}, {address.number}
+            </p>
+            <p>
+              {address.district} - {address.city}, {address.state}
+            </p>
+          </li>
+          <li>
+            <i>
+              <Timer weight="fill" />
+            </i>
+            <p>Previsão de entrega</p>
+            <p>20 min - 30 min </p>
+          </li>
+          <li>
+            <i>
+              <CurrencyDollar />
+            </i>
+            <p>Pagamento na entrega</p>
+            <p>{paymentType}</p>
+          </li>
+        </DeliveryInfoBox>
+        <div>
+          <img src="./src/assets/motoboy.svg" alt="" />
+        </div>
+      </DeliveryInfoContainer>
     </SuccessContainer>
   )
 }
--- src/pages/Success/styles.ts
+++ src/pages/Success/styles.ts
@@ -1,3 +1,7 @@
 import styled from 'styled-components'
 
 export const SuccessContainer = styled.main``
+
+export const DeliveryInfoContainer = styled.div``
+
+export const DeliveryInfoBox = styled.ul``
