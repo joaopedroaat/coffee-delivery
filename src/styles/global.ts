459e9a0fe4bd2fe17e05f7576f0c82dc97158790
joaopedroaat <jotapedro1231@gmail.com>
2023-05-02 10:32:02 -0300

    Added outline color

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -7,13 +7,16 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
-
+  
   body, input, textarea, button {
     font-family: 'Roboto', sans-serif;
     font-weight: 400;
     font-size: 1rem;
     color: ${(props) => props.theme['base-text']};
     line-height: 1.3;
+    :focus {
+      outline: 1px solid ${(props) => props.theme.yellow};
+    }
   }
 
   ul {
