c5ceeb14f9ec1c8e75f228d5e1e89c7cb3e3a1f5
joaopedroaat <jotapedro1231@gmail.com>
2023-05-02 10:19:00 -0300

    Media query for bigger screens

--- src/layout/DefaultLayout/styles.ts
+++ src/layout/DefaultLayout/styles.ts
@@ -3,4 +3,8 @@ import styled from 'styled-components'
 export const DefaultLayoutContainer = styled.div`
   padding: 0 10rem;
   padding-bottom: 10rem;
+
+  @media (min-width: 1660px) {
+    padding: 0 20rem;
+  }
 `
