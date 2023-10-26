96043c940839929f9126a0729a4ecff8ef4c0f46
João Pedro <jotapedro1231@gmail.com>
2023-10-26 13:45:43 -0300

    add cart indicator

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,31 @@
+import { NavLink } from 'react-router-dom'
 import { CoffeeList } from './components/CoffeeList'
 import { HomeInfo } from './components/HomeInfo'
-import { HomeContainer } from './styles'
+import { CartInfo, HomeContainer } from './styles'
+import { ShoppingCartSimple } from '@phosphor-icons/react'
+import { useContext } from 'react'
+import { CartContext } from '../../contexts/CartContext'
 
 export function Home() {
+  const { items } = useContext(CartContext)
+
   return (
     <HomeContainer>
       <HomeInfo />
       <CoffeeList />
+      {items.length && (
+        <NavLink to="checkout" title="Carrinho">
+          <CartInfo>
+            <ShoppingCartSimple size={24} />
+            <span>
+              {items
+                .reduce((acc, item) => item.quantity + acc, 0)
+                .toString()
+                .padStart(2, '0')}
+            </span>
+          </CartInfo>
+        </NavLink>
+      )}
     </HomeContainer>
   )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,40 @@
 import styled from 'styled-components'
 
-export const HomeContainer = styled.main``
+export const HomeContainer = styled.main`
+  position: relative;
+`
+
+export const CartInfo = styled.button`
+  position: fixed;
+  right: 2rem;
+  bottom: 2rem;
+
+  @media (max-width: 620px) {
+    right: 1rem;
+    bottom: 1rem;
+  }
+
+  padding: 1rem;
+  border-radius: 50%;
+  border: none;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: ${(props) => props.theme.yellow};
+  color: ${(props) => props.theme.white};
+
+  cursor: pointer;
+
+  span {
+    position: absolute;
+    top: -0.25rem;
+    right: -0.25rem;
+    font-size: 0.765rem;
+    background: ${(props) => props.theme['yellow-dark']};
+    padding: 0.45rem;
+    border-radius: 50%;
+    font-weight: bold;
+  }
+`
